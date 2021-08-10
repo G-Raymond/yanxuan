a543a0648d4b30440d83df3a24beaa4c75d394a1
Common Gao <common.gao@KUHAN.LAN>
2021-08-10 22:08:11 +0800

    我的九宫格

--- pages/mine/index.js
+++ pages/mine/index.js
@@ -4,62 +4,87 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    options: [{
+      icon: '',
+      name: "我的订单"
+    }, {
+      icon: '',
+      name: "优惠券"
+    }, {
+      icon: '',
+      name: "我的足迹"
+    }, {
+      icon: '',
+      name: "我的收藏"
+    }, {
+      icon: '',
+      name: "地址管理"
+    }, {
+      icon: '',
+      name: "联系客服"
+    }, {
+      icon: '',
+      name: "帮助中心"
+    }, {
+      icon: '',
+      name: "意见反馈"
+    }]
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
